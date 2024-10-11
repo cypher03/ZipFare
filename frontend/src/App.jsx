5013a4bcdc2669a6319c047294cef9cfb9b8c3a2
cypher03 <cypherking03@gmail.com>
2024-10-11 12:08:17 +0530

    LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-
+import navBar from './components/navBar.jsx'
+import landingPage from './components/landingPage.jsx'
 function App() {
 
   return (
     <>
+    <h1>dd</h1>
+    
     </>
   )
 }
---
+++ frontend/src/components/landingPage.jsx
@@ -0,0 +1,11 @@
+
+export default function landingPage(){
+    return(
+        <>
+        <h1>ddd</h1>
+        <Container maxWidth="sm">
+            <h1>hello</h1>
+        </Container>
+        </>
+    )
+}
